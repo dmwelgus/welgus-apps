bb420997eaad9eeb4fb0568da6a315cf3c0681ee
dmwelgus <dmwelgus@uchicago.edu>
2015-06-27 08:21:07 -0500

    Delete deploy.R

--- deploy.R
+++
@@ -1,4 +0,0 @@
-library(shinyapps)
-shinyapps::deployApp('C:/Users/dmwelgus/Documents/portal_apps/by_ca_year')
-shinyapps::deployApp('C:/Users/dmwelgus/Documents/portal_apps/by_month')
-shinyapps::deployApp('C:/Users/dmwelgus/Documents/portal_apps/year_to_date')
