a7a7c60a0b48178343d7eeebce879fabb0c7a82d
dmwelgus <dmwelgus@gmail.com>
2016-02-06 12:48:28 -0600

    add Data and deploy.R

---
+++ deploy.R
@@ -0,0 +1,4 @@
+library(rsconnect)
+rsconnect::deployApp('Y:/welgus-apps-master/by_ca_year')
+rsconnect::deployApp('Y:/welgus-apps-master/by_month')
+rsconnect::deployApp('Y:/welgus-apps-master/year_to_date')
