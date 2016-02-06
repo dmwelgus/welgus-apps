459d4fc7d370fbbe82887fe24e6afe7c13af2ec2
dmwelgus <dmwelgus@gmail.com>
2016-02-06 12:51:11 -0600

    Delete deploy.R

--- deploy.R
+++
@@ -1,4 +0,0 @@
-library(rsconnect)
-rsconnect::deployApp('Y:/welgus-apps-master/by_ca_year')
-rsconnect::deployApp('Y:/welgus-apps-master/by_month')
-rsconnect::deployApp('Y:/welgus-apps-master/year_to_date')
