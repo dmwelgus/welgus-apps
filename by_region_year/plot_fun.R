7e930f276b841a8ebbf0f2746ec1be0949776e1c
David Matthew Welgus <dmwelgus@UCHICAGO.EDU>
2016-08-24 23:09:56 -0500

    Changed map colors

--- by_region_year/plot_fun.R
+++ by_region_year/plot_fun.R
@@ -6,7 +6,7 @@ plot_fun <- function(fill_var, title, df) {
       geom_polygon() +
       geom_path(color = "black") +
       coord_equal() +
-      scale_fill_continuous(low = "#ffeda0", high = "#800026", na.value = "#ffffcc") +
+      scale_fill_continuous(low = "#edf8fb", high = "#810f7c", na.value = "#f7fcfd", guide = guide_colorbar(title = "Rate")) +
       theme(axis.text.x = element_blank(), axis.text.y = element_blank(), 
             axis.title.x = element_blank(), axis.title.y = element_blank(),
             axis.ticks = element_blank(), panel.background = element_rect(fill = "white"), panel.grid.major = element_line(colour = "white"),
@@ -20,7 +20,7 @@ plot_fun <- function(fill_var, title, df) {
       geom_polygon() +
       geom_path(color = "black") +
       coord_equal() +
-      scale_fill_continuous(low = "#ffeda0", high = "#800026", na.value = "#ffffcc") +
+      scale_fill_continuous(low = "#edf8fb", high = "#810f7c", na.value = "#f7fcfd", guide = guide_colorbar(title = "Rate")) +
       theme(axis.text.x = element_blank(), axis.text.y = element_blank(), 
             axis.title.x = element_blank(), axis.title.y = element_blank(),
             axis.ticks = element_blank(), panel.background = element_rect(fill = "white"), panel.grid.major = element_line(colour = "white"),
