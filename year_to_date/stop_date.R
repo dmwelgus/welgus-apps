d192162851a3904f7c028cc3ddbace73f315eec9
David Welgus <dmwelgus@gmail.com>
2018-03-17 16:19:22 -0500

    fixed date related bug

--- year_to_date/stop_date.R
+++ year_to_date/stop_date.R
@@ -2,11 +2,7 @@
 get_stopDate <- function() {
   
   # Quick query to get most recent date in crime table
-  ref_time <- Sys.time() - 20*60*60*24
-  ref_time <- gsub(" ", "T", ref_time)
-  ref_time <- paste("'",ref_time,"'", sep = "")
-  url <- "https://data.cityofchicago.org/resource/6zsd-86xi.json?$select=fbi_code,max(date)&$group=fbi_code&$where=date>=%s"
-  url <- sprintf(url, ref_time)
+  url <- "https://data.cityofchicago.org/resource/6zsd-86xi.json?$select=fbi_code,max(date)&$group=fbi_code"
   x <- fromJSON(url)
   
   stop_date <- max(x$max_date)
